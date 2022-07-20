b1b59f40fa7982c4d6474986e2f52266eb09ede8
Gabriel Matheus <gabrielpessoal626@gmail.com>
2022-07-20 11:45:37 -0300

    feature: add initial structure of the project

---
+++ controlsy/Sources/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  controlsy
+//
+//  Created by Gabriel Matheus on 20/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
