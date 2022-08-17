05534d392f29bf4cea24a2f5b66ffa20196868c8
Gabriel Matheus <gabrielpessoal626@gmail.com>
2022-08-17 17:53:45 -0300

    create initial structure of onboard screen

---
+++ controlsy/Sources/App Sections/OnBoarding/View/OnBoardingViewController.swift
@@ -0,0 +1,15 @@
+//
+//  OnBoardingViewController.swift
+//  controlsy
+//
+//  Created by Gabriel Matheus on 16/08/22.
+//
+
+import Foundation
+import UIKit
+
+class OnBoardingViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
