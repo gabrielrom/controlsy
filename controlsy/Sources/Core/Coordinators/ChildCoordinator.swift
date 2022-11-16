3fc05fdf2b0ffb44873f0e44e7124067531b2335
Gabriel Matheus <gabrielpessoal626@gmail.com>
2022-11-16 01:28:09 -0300

    :sparkles: feat: add a default implementation to clean method

--- controlsy/Sources/Core/Coordinators/ChildCoordinator.swift
+++ controlsy/Sources/Core/Coordinators/ChildCoordinator.swift
@@ -7,9 +7,34 @@
 
 import Foundation
 import UIKit
+import RxRelay
+import RxSwift
+
+enum Step {
+    case stop
+    case none
+}
 
 protocol ChildCoordinatorProtocol {
-    var navigation: UINavigationController { get }
-    var mainCoordinator: MainCoordinator? { get }
+    var uniqueIdentifier: UUID { get }
+    var navigation: UINavigationController? { get set }
+    var rootViewController: UIViewController? { get set }
+    var mainCoordinator: MainCoordinator? { get set }
+    var selectedStep: BehaviorRelay<Step> { get }
+    var disposeBag: DisposeBag? { get set }
+    
     func start()
+}
+
+extension ChildCoordinatorProtocol {
+    mutating func clean() {
+        guard let rootViewController = rootViewController else { return }
+
+        rootViewController.removeFromParent()
+
+        self.disposeBag = nil
+        self.navigation = nil
+        self.rootViewController = nil
+        self.mainCoordinator = nil
+    }
 }
